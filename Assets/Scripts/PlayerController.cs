8ae4aa54b2acf27a976c4c9a7b55a08ffd99ef63
aidancapaldi <capaldi.a@husky.neu.edu>
2023-10-04 18:51:08 -0400

    Tried out lecture prefab and business woman, reviewed scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,74 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerController : MonoBehaviour
+// {
+//     // Player speed
+//     public float moveSpeed = 2.0f;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         KeyboardMovement();
+//     }
+
+//     // Handle keyboard input from the player and move the player
+//     void KeyboardMovement()
+//     {
+//         Vector3 dir = new Vector3(0, 0, 0);
+//         dir.x = Input.GetAxis("Horizontal");
+//         dir.z = -Input.GetAxis("Vertical");
+//         transform.Translate(dir * moveSpeed * Time.deltaTime);
+//     }
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Player speed
-    public float moveSpeed = 2.0f;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        KeyboardMovement();
-    }
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
 
-    // Handle keyboard input from the player and move the player
-    void KeyboardMovement()
-    {
-        Vector3 dir = new Vector3(0, 0, 0);
-        dir.x = Input.GetAxis("Horizontal");
-        dir.z = Input.GetAxis("Vertical");
-        transform.Translate(dir * moveSpeed * Time.deltaTime);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // if (move != Vector3.zero)
+        // {
+        //     gameObject.transform.forward = move;
+        // }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
---
+++ Assets/StudioNewPunch/BusinessWoman/Scripts/BWomanInstantiate.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BWomanInstantiate : MonoBehaviour {
+	private int hairCol;
+	private int hairTyp;
+	private int faceTyp;
+	private int eyeCol;
+	private int topCol;
+	private int btmCol;
+	private int skinTyp;
+	private int topTyp;
+	private int btmTyp;
+	private int jacketCol;
+	private int shirtCol;
+	private int skirtCol;
+	private int pantsCol;
+	private int shoesCol;
+
+
+
+	public enum ShoesColor
+	{
+		Blue,
+		Black,
+		Gray,
+		LightGray,
+		Red,
+		White
+
+	}
+	public enum JacketColor
+	{
+		Blue,
+		Black,
+		Gray,
+		LightGray,
+		Red,
+		White
+
+	}
+
+	public enum SkirtColor
+	{
+		Blue,
+		Black,
+		Gray,
+		LightGray,
+		Red,
+		White
+	}
+
+	public enum PantsColor
+	{
+		Blue,
+		Black,
+		Gray,
+		LightGray,
+		Red,
+		White
+
+	}
+
+	public enum ShirtColor
+	{
+		Blue,
+		Black,
+		Gray,
+		LightBlue,
+		Red,
+		White
+
+	}
+
+
+
+
+
+	public enum HairType
+	{
+		Medium,
+		PonyTail,
+		FrenchRoll,
+		Short,
+		Bun
+
+	}
+
+	public enum HairColor
+	{
+		Blond,
+		White,
+		Dark,
+		Red,
+		Brown
+
+	}
+
+	public enum EyeColor
+	{
+		Brown,
+		Blue,
+		Green,
+		Black,
+		DarkBlue,
+		LightBrown
+
+	}
+
+
+	public enum FaceType
+	{
+		FaceA,
+		FaceB,
+		FaceC,
+		FaceD,
+		FaceE
+
+	}
+
+	public enum SkinType
+	{
+		Pink,
+		Black,
+		White,
+		Tanned,
+		Pale,
+		Brown
+
+	}
+
+
+	public enum TopColors
+	{
+		WhiteBlue,
+		Blue,
+		Grey,
+		WhitePurple
+
+	}
+
+	public enum TopType
+	{
+		Shirt,
+		Jacket
+
+	}
+
+	public enum BottomType
+	{
+		Skirt,
+		Pants
+
+	}
+
+
+	public Transform prefabObject;
+
+	public FaceType faceType;
+	public SkinType skinType;
+	public EyeColor eyeColor;
+	public TopType topType;
+	public JacketColor jacketColor;
+	public ShirtColor shirtColor;
+	public BottomType bottomType;
+	public PantsColor pantsColor;
+	public SkirtColor skirtColor;
+
+	public HairType hairType;
+	public HairColor hairColor;
+	public ShoesColor shoesColor;
+	//public GameObject currentFace;
+
+	// Use this for initialization
+	void Start () {
+		Transform pref = Instantiate (prefabObject, gameObject.transform.position, gameObject.transform.rotation);
+		hairCol = (int)hairColor;
+		eyeCol = (int)eyeColor;
+		hairTyp = (int)hairType;
+		faceTyp = (int)faceType;
+		btmTyp = (int)bottomType;
+		topTyp = (int)topType;
+		skinTyp = (int)skinType;
+		jacketCol = (int)jacketColor;
+		shirtCol = (int)shirtColor;
+		skirtCol = (int)skirtColor;
+		pantsCol = (int)pantsColor;
+		shoesCol = (int)shoesColor;
+		pref.gameObject.GetComponent<BWomanCustomise> ().charCustomize(faceTyp, eyeCol, topTyp, btmTyp, hairTyp, hairCol, skinTyp, jacketCol, shirtCol, skirtCol, pantsCol, shoesCol);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
