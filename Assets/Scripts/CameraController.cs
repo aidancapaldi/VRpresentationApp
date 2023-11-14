652cfdddaff1e405bfe79ccbb1f4c6fabcc2da2a
aidancapaldi <capaldi.a@husky.neu.edu>
2023-11-14 12:36:52 -0500

    Fixed camera and player movement, still working on collision

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float turnSpeed = 4.0f;
+    public float sensitivity = 1000f;
+    public float xRotation = 0f;
+    private Transform player;
+    private Quaternion localRotate;
 
-    public float minTurnAngle = -90.0f;
-    public float maxTurnAngle = 90.0f;
-    private float rotX;
+    void Start()
+    {
+        // Cursor should stay locked the the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     void Update()
     {
@@ -16,13 +22,16 @@ public class CameraController : MonoBehaviour
     }
     void MouseAiming()
     {
-        // get the mouse inputs
-        float y = Input.GetAxis("Mouse X") * turnSpeed;
-        rotX += Input.GetAxis("Mouse Y") * turnSpeed;
-        // clamp the vertical rotation
-        rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
-        // rotate the camera
-        transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
-    }
+        // Get the directional inputs, smooth to time
+        float MX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float MY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
+        xRotation -= MY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate the player's forward direction to be that of the camera
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        localRotate = transform.localRotation;
+        player.Rotate(Vector3.up * MX);
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,74 +1,79 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class PlayerController : MonoBehaviour
-// {
-//     // Player speed
-//     public float moveSpeed = 2.0f;
-
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         KeyboardMovement();
-//     }
-
-//     // Handle keyboard input from the player and move the player
-//     void KeyboardMovement()
-//     {
-//         Vector3 dir = new Vector3(0, 0, 0);
-//         dir.x = Input.GetAxis("Horizontal");
-//         dir.z = -Input.GetAxis("Vertical");
-//         transform.Translate(dir * moveSpeed * Time.deltaTime);
-//     }
-// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    // Player speed
+    public float moveSpeed = 2.0f;
 
-    private void Start()
+    private Rigidbody rBody;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
+        rBody = GetComponent<Rigidbody>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+        KeyboardMovement();
+    }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        // if (move != Vector3.zero)
-        // {
-        //     gameObject.transform.forward = move;
-        // }
-
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+    // Handle keyboard input from the player and move the player
+    void KeyboardMovement()
+    {
+        Vector3 dir = new Vector3(0, 0, 0);
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+        //rBody.MovePosition(dir * moveSpeed * Time.deltaTime);
     }
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerController : MonoBehaviour
+// {
+//     private CharacterController controller;
+//     private Vector3 playerVelocity;
+//     private bool groundedPlayer;
+//     private float playerSpeed = 2.0f;
+//     private float jumpHeight = 1.0f;
+//     private float gravityValue = -9.81f;
+
+//     private void Start()
+//     {
+//         controller = gameObject.GetComponent<CharacterController>();
+//     }
+
+//     void Update()
+//     {
+//         groundedPlayer = controller.isGrounded;
+//         if (groundedPlayer && playerVelocity.y < 0)
+//         {
+//             playerVelocity.y = 0f;
+//         }
+
+//         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+//         controller.Move(move * Time.deltaTime * playerSpeed);
+
+//         // if (move != Vector3.zero)
+//         // {
+//         //     gameObject.transform.forward = move;
+//         // }
+
+//         // Changes the height position of the player..
+//         if (Input.GetButtonDown("Jump") && groundedPlayer)
+//         {
+//             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+//         }
+
+//         playerVelocity.y += gravityValue * Time.deltaTime;
+//         controller.Move(playerVelocity * Time.deltaTime);
+//     }
+// }
